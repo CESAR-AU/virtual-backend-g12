d2476b00f192d7e1c5f4f4134ebe9afd4f6519cb
CESAR-AU <cesaraugusto@c-innova.pe>
2022-02-28 23:06:02 -0500

    Mas

---
+++ Dia_1/04-op-asignaciones.py
@@ -0,0 +1,20 @@
+#operadores de asignacion
+n1,n2 = 10, 20
+# IGUAL =
+n1 = 10
+print(n1)
+#INCREMENTO ++
+n1 +=1
+print(n1)
+#DECREMENTOS --
+n1 -=1
+print(n1)
+#MULTIPLIVADOR
+n1 *=2
+print(n1)
+#DIVIDENDO
+n1 /=2
+print(n1)
+#POTENCIA
+n1 **=2
+print(n1)
