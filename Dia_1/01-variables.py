2da1bafb0db9092b5225b922e6c6fdca92c6c7ed
CESAR-AU <cesaraugusto@c-innova.pe>
2022-02-28 21:30:40 -0500

    declaracion de los primeros archivos

---
+++ Dia_1/01-variables.py
@@ -0,0 +1,12 @@
+#comentarios
+# TODO: logica validaciones
+
+nombre = "cesar"
+edad = 30
+
+# texto de varias lineas
+descripcion = '''Hola
+line
+otra linea'''
+
+print('a', 'b', 'c')
---
+++ Dia_1/02-impresion-pantalla.py
@@ -0,0 +1,13 @@
+nombre = "Juan"
+
+print(nombre)
+
+#
+print('El nombre:', nombre, "usuario")
+
+estado_civil = 'viudo'
+
+#
+
+print('La persona {} es {}'.format(nombre, estado_civil))
+print('{1} es una persona {0}'.format(estado_civil, nombre))
