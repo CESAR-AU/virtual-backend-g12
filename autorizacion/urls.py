bf0d652aa8c5a3e9616e11ea7a491c49e12074c8
Cesar AU <cesaraugusto@c-innova.pe>
2022-04-25 22:34:50 -0500

    Semana 09 inicio

--- autorizacion/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import RegistroUsuarioApiView
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-urlpatterns = [
-    path('registro/', RegistroUsuarioApiView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),
-]
--- fact_electr/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import GenerarComprobanteApiView
-
-urlpatterns = [
-    path('generar_comprobante', GenerarComprobanteApiView.as_view()),
-]
