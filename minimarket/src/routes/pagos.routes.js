eb7c2d81e67b79fc47926fb0dca1b5f19c5457b1
Cesar AU <cesaraugusto@c-innova.pe>
2022-05-04 22:51:35 -0500

    Semana 10

--- minimarket/src/routes/pagos.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { crearPreferencia, mercadoPagoWebhooks } from "../controllers/pagos.controller.js";
-
-export const pagosRouter =  Router();
-
-pagosRouter.post("/generar-pago", crearPreferencia);
-pagosRouter.post('/mp-webhooks', mercadoPagoWebhooks);
